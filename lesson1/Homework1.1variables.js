1a15639154e1e6f1bd5a54c93bc33591f989b21b
solserj <fgump259@gmail.com>
2024-11-04 18:37:35 +0200

    I`ve done Homework1: Homework1.1variables.html, Homework1.1variables.js, Homework1.2FirstName.html, Homework1.2FirstName.js, Homework3.1Typeof.html, Homework3.1Typeof.js.

---
+++ lesson1/Homework1.1variables.js
@@ -0,0 +1,17 @@
+// Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+//     Вивести кожну змінну за допомогою: console.log
+let greeting, owner, domain, region, oddNumber, evenNumber, negativeNumber, positiveNumber, floatNumber, integerNumber, booleanType1, booleanType2;
+greeting = 'hello';
+owner = 'owu';
+domain = 'com';
+region = 'ua';
+oddNumber = 1;
+evenNumber = 10;
+negativeNumber = -999;
+positiveNumber = 123;
+const PI = 3.14;
+floatNumber = 2.7;
+integerNumber = 16;
+booleanType1 = true;
+booleanType2 = false;
+console.log(greeting, owner, domain, region, oddNumber, evenNumber, negativeNumber, positiveNumber, floatNumber, integerNumber, booleanType1, booleanType2, PI);
---
+++ lesson1/Homework1.2FirstName.js
@@ -0,0 +1,6 @@
+let firstName, middleName, lastName, person;
+firstName = 'Serhii';
+middleName = 'Mykolayovych';
+lastName = 'Soloviov';
+person = lastName + ' ' + firstName + ' ' + middleName;
+// Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
